78f7a1f1590ba1f8989b178eff4c52df901641f3
Harshit <harshit.ravi18@gmail.com>
2026-03-12 14:12:15 +0100

    feat: update next.config.mjs for static export and add .nojekyll file

--- next.config.mjs
+++ next.config.mjs
@@ -1,3 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = { reactStrictMode: true };
+const nextConfig = {
+  reactStrictMode: true,
+  output: "export", // This creates a static 'out' folder when you build
+  images: {
+    unoptimized: true,
+  },
+
+  basePath: "/repo-name",
+};
+
 export default nextConfig;
